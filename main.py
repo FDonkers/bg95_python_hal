48cfc3260f102e63663ab6268496210c87f72511
felix donkers <info@felixdonkers.nl>
2024-05-30 16:51:17 +0200

    improved test cases

--- main.py
+++ main.py
@@ -12,6 +12,50 @@ logging.basicConfig(format='%(asctime)s.%(msecs)03d - %(name)s - %(levelname)s -
 
 my_bg95 = osi_layer(logging)
 
+def send_message_via_http_get():
+  total_time_http_get = 0
+  my_timer.start()
+  status, response = my_bg95.HTTP_GET("http://postman-echo.com/get/?foo1=bar1")
+  total_time_http_get += my_timer.time_passed()
+  logging.info(f"Average time http_get: {total_time_http_get/(i+1)} seconds")
+  if status:
+    logging.info(f"HTTP_GET PASSED! with response:\n{response["result"]}\n===payload start===\n{response["payload"]}\n===payload end===")
+  else:
+    logging.error(f"HTTP_GET FAILED!")
+
+def send_message_via_https_get():
+  total_time_https_get = 0
+  my_timer.start()
+  status, response = my_bg95.HTTPS_GET("https://postman-echo.com/get/?foo1=bar1")
+  total_time_https_get += my_timer.time_passed()
+  logging.info(f"Average time https_get: {total_time_https_get/(i+1)} seconds")
+  if status:
+    logging.info(f"HTTPS_GET PASSED! with response:\n{response["result"]}\n===payload start===\n{response["payload"]}\n===payload end===")
+  else:
+    logging.error(f"HTTPS_GET FAILED!")
+
+def send_message_via_http_post():
+  total_time_http_post = 0
+  my_timer.start()
+  status, response = my_bg95.HTTP_POST("http://postman-echo.com/post/", "foo1=bar1")
+  total_time_http_post += my_timer.time_passed()
+  logging.info(f"Average time http_post: {total_time_http_post/(i+1)} seconds")
+  if status:
+    logging.info(f"HTTP_POST PASSED! with response:\n{response["result"]}\n===payload start===\n{response["payload"]}\n===payload end===")
+  else:
+    logging.error(f"HTTP_POST FAILED!")
+
+def send_message_via_https_post():
+  total_time_https_post = 0
+  my_timer.start()
+  status, response = my_bg95.HTTPS_POST("https://postman-echo.com/post/", "foo1=bar1")
+  total_time_https_post += my_timer.time_passed()
+  logging.info(f"Average time https_post: {total_time_https_post/(i+1)} seconds")
+  if status:
+    logging.info(f"HTTPS_POST PASSED! with response:\n{response["result"]}\n===payload start===\n{response["payload"]}\n===payload end===")
+  else:
+    logging.error(f"HTTPS_POST FAILED!")
+
 if __name__ == "__main__":
   my_timer = timer(logging)
 
@@ -21,69 +65,25 @@ if __name__ == "__main__":
     print("FAILED TO OPEN USB CONNECTION")
     exit()
 
+  my_bg95.connect_modem_to_network()
 
-  total_time_http_get = 0
-  total_time_https_get = 0
-  total_time_http_post = 0
-  total_time_https_post = 0
-  for i in range(500):
+  for i in range(365):
     logging.info(f"***** Loop {i}")
 
-    my_bg95.connect_modem_to_network()
-    # my_bg95.request_network_info()
+    my_bg95.request_network_info()
 
-    # ##### HTTP_GET
-    # my_timer.start()
-    # status, response = my_bg95.HTTP_GET("http://postman-echo.com/get/?foo1=bar1")
-    # total_time_http_get += my_timer.time_passed()
-    # logging.info(f"Average time http_get: {total_time_http_get/(i+1)} seconds")
-    # if status:
-    #   logging.info(f"HTTP(S)_GET/POST PASSED! with response:\n{response["result"]}\n===payload start===\n{response["payload"]}\n===payload end===")
-    # else:
-    #   logging.error(f"HTTP(S)_GET/POST FAILED!")
+    send_message_via_http_get()
+    time.sleep(1)
+    send_message_via_https_get()
+    time.sleep(1)
+    send_message_via_http_post()
+    time.sleep(1)
+    send_message_via_https_post()
+    time.sleep(10) 
 
-    # time.sleep(1)
-
-    ##### HTTPS_GET
-    # my_timer.start()
-    # status, response = my_bg95.HTTPS_GET("https://postman-echo.com/get/?foo1=bar1")
-    # total_time_https_get += my_timer.time_passed()
-    # logging.info(f"Average time https_get: {total_time_https_get/(i+1)} seconds")
-    # if status:
-    #   logging.info(f"HTTP(S)_GET/POST PASSED! with response:\n{response["result"]}\n===payload start===\n{response["payload"]}\n===payload end===")
-    # else:
-    #   logging.error(f"HTTP(S)_GET/POST FAILED!")
-
-    # time.sleep(1)
-
-    ##### HTTP_POST
-    # my_timer.start()
-    # status, response = my_bg95.HTTP_POST("http://postman-echo.com/post/", "foo1=bar1")
-    # total_time_http_post += my_timer.time_passed()
-    # logging.info(f"Average time http_post: {total_time_http_post/(i+1)} seconds")
-    # if status:
-    #   logging.info(f"HTTP(S)_GET/POST PASSED! with response:\n{response["result"]}\n===payload start===\n{response["payload"]}\n===payload end===")
-    # else:
-    #   logging.error(f"HTTP(S)_GET/POST FAILED!")
-
-    # time.sleep(1)
-
-    ##### HTTPS_POST
-    my_timer.start()
-    status, response = my_bg95.HTTPS_POST("https://postman-echo.com/post/", "foo1=bar1")
-    total_time_https_post += my_timer.time_passed()
-    logging.info(f"Average time https_post: {total_time_https_post/(i+1)} seconds")
-    if status:
-      logging.info(f"HTTP(S)_GET/POST PASSED! with response:\n{response["result"]}\n===payload start===\n{response["payload"]}\n===payload end===")
-    else:
-      logging.error(f"HTTP(S)_GET/POST FAILED!")
-
-    my_bg95.disconnect_modem_from_network()
-
-    time.sleep(100)
-
-  # ##### READ TIMER
+  my_bg95.disconnect_modem_from_network()
 
   my_bg95.close_usb()
 
   logging.debug("\n******************************")
+ 
